32dbc5d070068b146c5303e0eaaeeb1b93670b5a
Jacqui Maher and Wilson Bilkovich <dev+jacqui+wilson@hashrocket.com>
2009-01-19 12:13:36 -0500

    APP_CONFIG, not SETTINGS in settings.yml

--- config/initializers/sunlight.rb
+++ config/initializers/sunlight.rb
@@ -1,4 +1,4 @@
-if defined?(SETTINGS) && key = SETTINGS[:sunlight_api_key]
+if defined?(APP_CONFIG) && key = APP_CONFIG[:sunlight_api_key]
   Sunlight.api_key = key
 else
   warn "No Sunlight API key found. Check config/settings.yml"
