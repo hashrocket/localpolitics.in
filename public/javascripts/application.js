5d89a194a8bf1fecd793a8811123dc53673b4879
Jon Larkowski and Tim Pope <dev+l4rk+tpope@hashrocket.com>
2009-01-19 17:46:39 -0500

    Bare bones layout with jQuery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,15 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+jQuery(document).ready(function($){
+});
+
+jQuery(document).ajaxSend(function(event, request, settings) {
+  if(typeof(AUTH_TOKEN) == "undefined") return;
+  if(settings.type == "GET") return;
+  if(settings.contentType != "application/x-www-form-urlencoded") return;
+  if(settings.data)
+    settings.data += "&";
+  else {
+    request.setRequestHeader("Content-Type",settings.contentType);
+    settings.data = "";
+  }
+  settings.data += "authenticity_token=" + encodeURIComponent(AUTH_TOKEN);
+});
