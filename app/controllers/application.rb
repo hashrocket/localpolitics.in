23c653c8e43421cbb5eec84a38a0883764c7314c
Jacqui Maher and Wilson Bilkovich <dev+jacqui+wilson@hashrocket.com>
2009-01-19 15:20:40 -0500

    Clean whitespace!

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '8fde19bfc9b4608247e3e7b8b0f29803'
-  
+
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
