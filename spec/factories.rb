af878250279da9f0bcf41c66712c8a6c199a7832
Jacqui Maher and Veez (Matt Remsik) <dev+jacqui+veez@hashrocket.com>
2009-03-17 14:28:38 -0400

    Added factory for Bill

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,13 @@
 Factory.sequence(:user_email) { |n| "user_#{n}@example.com" }
 
+Factory.define Bill do |b|
+  b.sponsor_id    1234
+  b.cosponsor_ids [1,2,3,4]
+  b.title         "I'm just a bill"
+  b.description   "... sitting on Capitol Hill"
+  b.summary       "Well, it's a long, long journey to the capital city.  It's a long, long wait While I'm sitting in committee, But I know I'll be a law someday."
+end
+
 Factory.define User do |u|
   u.email { Factory.next(:user_email) }
 end
