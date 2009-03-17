f1eae79506b9e4bac5b9980d3a2ae5cde637d3d6
Jacqui Maher and Veez (Matt Remsik) <dev+jacqui+veez@hashrocket.com>
2009-03-17 14:32:02 -0400

    Bill#sponsor_name and Bill#cosponsor_names

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,4 +1,15 @@
 class Bill < ActiveRecord::Base
   validates_presence_of :title, :sponsor_id
   serialize :cosponsor_ids
+
+  def sponsor_name
+    if congress_person = CongressPerson.find(:govtrack_id => sponsor_id)
+      congress_person.full_name
+    end
+  end
+
+  def cosponsor_names
+    return if cosponsor_ids.blank?
+    CongressPerson.find_all_by_govtrack_id(cosponsor_ids).map(&:full_name).join(", ")
+  end
 end
--- spec/models/bill_spec.rb
+++ spec/models/bill_spec.rb
@@ -16,12 +16,50 @@ describe Bill do
     end
   end
 
-  describe "cosponsors" do
+  describe "cosponsor_ids" do
     before do
       @bill.cosponsor_ids = %w(1 2 3)
     end
     it "returns an array of cosponsors" do
       @bill.cosponsor_ids.should == %w(1 2 3)
+    end
+  end
+
+  describe "#cosponsor_names" do
+    before do
+      @bill = Factory(:bill)
+      @cp1 = stub('congress_person', :full_name => "Bill Nelson")
+      @cp2 = stub('congress_person', :full_name => "Russ Feingold")
+    end
+    it "returns a comma delimited list of congress person names" do
+      CongressPerson.stubs(:find_all_by_govtrack_id).returns([@cp1,@cp2])
+      @bill.cosponsor_names.should == "Bill Nelson, Russ Feingold"
+    end
+    it "returns nil if no cosponsor_ids set" do
+      CongressPerson.stubs(:find_all_by_govtrack_id)
+      @bill.cosponsor_ids = nil
+      @bill.cosponsor_names.should be_nil
+    end
+  end
+
+  describe "#sponsor_name" do
+    before do
+      @bill = Factory(:bill)
+      stub_out_legislator_get_json_data
+      CongressPerson.stubs(:find)
+    end
+
+    it "finds a congress person by govtrack_id" do
+      CongressPerson.expects(:find).with(:govtrack_id => @bill.sponsor_id)
+      @bill.sponsor_name
+    end
+    it "returns the congress person's name" do
+      CongressPerson.stubs(:find).returns(mock('congress_person', :full_name => "Bill Nelson"))
+      @bill.sponsor_name.should == "Bill Nelson"
+    end
+    it "returns nil when no congress person found" do
+      CongressPerson.stubs(:find).returns(nil)
+      @bill.sponsor_name.should be_nil
     end
   end
 end
