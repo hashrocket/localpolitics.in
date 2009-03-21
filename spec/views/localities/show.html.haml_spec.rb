b9ff0588dc559756ee25f93c6f2e17324d4e40a0
Jim Remsik and Veez (Matt Remsik) <dev+bigtiger+veez@hashrocket.com>
2009-03-21 13:14:03 -0400

    Don't display links or images for incomplete representatives

--- spec/views/localities/show.html.haml_spec.rb
+++ spec/views/localities/show.html.haml_spec.rb
@@ -2,14 +2,32 @@ require File.dirname(__FILE__) + '/../../spec_helper.rb'
 
 describe "localities/show" do
   before do
-    Legislator.stubs(:all_for).returns({:senior_senator => fake_legislator, :junior_senator => fake_legislator, :representative => fake_legislator})
+    representative_hash = {:senior_senator => fake_legislator, :junior_senator => fake_legislator, :representative => fake_legislator}
+    Legislator.stubs(:all_for).returns(representative_hash)
+    @representative = CongressPerson.new(fake_legislator)
     @locality = Locality.new "53716"
+    @locality.stubs(:representative).returns(@representative)
     assigns[:locality] = @locality
     assigns[:party_totals] = {:R => 5, :D => 6}
     template.stubs(:preferred_party_text).returns("You're crazy democratic!")
   end
   it "should render without errors" do
     do_render
+  end
+  it "links to the congress person show page" do
+    do_render
+    response.should have_tag("a[href=?]", congress_person_path(@representative.crp_id))
+  end
+  it "doesn't link to the congress person show page if their is no crp_id" do
+    @representative.stubs(:crp_id).returns("")
+    @representative.stubs(:full_name).returns("something reasonable")
+    do_render
+    response.should_not have_tag("a", @representative.full_name)
+  end
+  it "doesn't list an image for a congress person with no crp_id" do
+    @representative.stubs(:has_photo?).returns(false)
+    do_render
+    response.should_not have_tag("img[src=?]", "/images/congresspeople/#{@representative.photo_id}.jpg")
   end
   describe "preferred party widget" do
     it "uses the correct div" do
