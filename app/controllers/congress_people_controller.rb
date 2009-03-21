4e29ad4474e71dadfd5e7116bcb3935050ab4c99
Dennis Eusebio <dennis@thoughtandtheory.com>
2009-03-21 18:50:43 -0400

    congress person detail page, round 1

--- app/controllers/congress_people_controller.rb
+++ app/controllers/congress_people_controller.rb
@@ -1,5 +1,6 @@
 class CongressPeopleController < ApplicationController
   def show
+    @tweet_time = Time.now
     @congress_person = CongressPerson.new(Legislator.where(:crp_id => params[:id]))
   end
 end
