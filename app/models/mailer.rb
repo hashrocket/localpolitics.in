2d8998314292c5cfe5d7f9b35bd7221c11c053dd
Tim Pope and Veez (Matt Remsik) <dev+tpope+veez@hashrocket.com>
2009-03-27 10:22:25 -0400

    Correct return address for twitter invite

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -4,7 +4,8 @@ class Mailer < ActionMailer::Base
 
   def twitter_invite(congress_person, args = {})
     recipients congress_person.email_address
-    from       args[:from]
+    from       "noreply@localpolitics.in"
+    reply_to   args[:citizen_email]
     subject    args[:subject]
     citizen_name = args[:citizen_name] || "A citizen"
     body       :congress_person => congress_person, :personal_message => args[:personal_message], :citizen_name => citizen_name, :invitation_text => Tweet.invitation_text
